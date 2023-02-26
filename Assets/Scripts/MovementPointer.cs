398e715bf705c35cec5739e2f0ff3d3422e1245c
Anton Siadun <antochaleo@gmail.com>
2023-02-26 16:20:02 +0300

    feat: add movement pointer script
    Detects nodes in all directions, and has flags that inform about possibility to move in these directions.

---
+++ Assets/Scripts/MovementPointer.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class MovementPointer : MonoBehaviour
+{
+    public bool CanMoveUp;
+    public bool CanMoveDown;
+    public bool CanMoveLeft;
+    public bool CanMoveRight;
+
+    public GameObject LeftNode;
+    public GameObject RightNode;
+    public GameObject UpNode;
+    public GameObject DownNode;
+
+    private void Start()
+    {
+        CheckDownDirection();
+        CheckLeftDirection();
+        CheckRightDirection();
+        CheckUpDirection();
+    }
+
+    private void CheckUpDirection()
+    {
+        var direction = Vector2.up;
+        RaycastHit2D[] raycastHits = Physics2D.RaycastAll(transform.position, direction);
+        foreach(var hit in raycastHits)
+        {
+            float distanceY = Mathf.Abs(transform.position.y - hit.point.y);
+            if(distanceY < 0.4f)
+            {
+                CanMoveUp = true;
+                UpNode = hit.collider.gameObject;
+            }
+        }
+    }
+
+    private void CheckDownDirection()
+    {
+        var direction = Vector2.down;
+        RaycastHit2D[] raycastHits = Physics2D.RaycastAll(transform.position, direction);
+        foreach (var hit in raycastHits)
+        {
+            float distanceY = Mathf.Abs(transform.position.y - hit.point.y);
+            if (distanceY < 0.4f)
+            {
+                CanMoveDown = true;
+                DownNode = hit.collider.gameObject;
+            }
+        }
+    }
+
+    private void CheckLeftDirection()
+    {
+        var direction = Vector2.left;
+        RaycastHit2D[] raycastHits = Physics2D.RaycastAll(transform.position, direction);
+        foreach (var hit in raycastHits)
+        {
+            float distanceY = Mathf.Abs(transform.position.x - hit.point.x);
+            if (distanceY < 0.4f)
+            {
+                CanMoveLeft = true;
+                LeftNode = hit.collider.gameObject;
+            }
+        }
+    }
+
+    private void CheckRightDirection()
+    {
+        var direction = Vector2.right;
+        RaycastHit2D[] raycastHits = Physics2D.RaycastAll(transform.position, direction);
+        foreach (var hit in raycastHits)
+        {
+            float distanceY = Mathf.Abs(transform.position.x - hit.point.x);
+            if (distanceY < 0.4f)
+            {
+                CanMoveRight = true;
+                RightNode = hit.collider.gameObject;
+            }
+        }
+    }
+}
