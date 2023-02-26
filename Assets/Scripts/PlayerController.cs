f914e4cd5565b7d69cc0c8ee9a0018b8d6e715e4
Anton Siadun <antochaleo@gmail.com>
2023-02-26 20:53:14 +0300

    feat: add player animation
    -//-

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _renderer;
+
     private MovementController _movementController;
 
     private void Awake()
@@ -11,6 +14,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        _animator.SetBool("IsMoving", true);
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             _movementController.SetDirection("left");
@@ -27,5 +31,28 @@ public class PlayerController : MonoBehaviour
         {
             _movementController.SetDirection("down");
         }
+
+        bool flipX =false, flipY = false;
+        if(_movementController.LastMovingDirection == "up")
+        {
+            _animator.SetInteger("Direction",1);
+        }
+        else if (_movementController.LastMovingDirection == "down")
+        {
+            _animator.SetInteger("Direction", 1);
+            flipY = true;
+        }
+        else if (_movementController.LastMovingDirection == "left")
+        {
+            _animator.SetInteger("Direction", 0);
+        }
+        else if (_movementController.LastMovingDirection == "right")
+        {
+            _animator.SetInteger("Direction", 0);
+            flipX = true;
+        }
+        _renderer.flipX = flipX;
+        _renderer.flipY = flipY;
+
     }
 }
